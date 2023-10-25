c0b896c17f2464572df1347a709c8106d0ac5a53
kamila1335 <110620556+kamila1335@users.noreply.github.com>
2023-10-25 11:26:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
    const instrumento = tecla.classList[1];
    const idAudio = `#som_${instrumento}`; //template string
 
-    tecla.onclick = function () {
+    tecla.onmousemove = function () {
         tocaSom(idAudio);
     }
      
